9cc7c30d20bcad86bc45c52655c0b24f78af4f97
salah9abdelrahman <salah9abdelrahman@outlook.com>
2021-08-15 07:22:00 +0200

    try i18n on restful

--- src/main/java/com/salah/ask/controller/TestController.java
+++ src/main/java/com/salah/ask/controller/TestController.java
@@ -1,15 +1,25 @@
 package com.salah.ask.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 @RequestMapping("/test")
 public class TestController {
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping
-    public String helloWorld(){
-        return "<h1> hello world <h1/>";
+    public String helloWorld(
+//            @RequestHeader(name = "Accept-Language", required = false) Locale locale
+    ) {
+        return "<h1>" + messageSource.getMessage("i18n.hello", null, LocaleContextHolder.getLocale()) + "<h1/>";
     }
 }
